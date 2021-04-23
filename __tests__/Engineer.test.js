77493bcf2a395e8f4d0c5b69c1063ee784705926
peter <ptmartinezsound@gmail.com>
2021-04-23 12:18:36 -0500

    completed engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,19 @@
-const Engineer = require('../lib/Engineer'); 
+const Engineer = require('../lib/Engineer');
 
+test("Setting Github account with constructor", () => {
+    const testHub = "GitUserName";
+    const employee1 = new Engineer("Quigonn", 4, "testing@test.com", testHub);
+    expect(employee1.github).toBe(testHub);
+});
 
+test("getRole() returns \"Engineer\"", () => {
+    const testName = "Engineer";
+    const employee1 = new Engineer("Yoda", 2, "testing@test.com", testName);
+    expect(employee1.getRole()).toBe(testName);
+});
 
-
-
-// test('creates a Engineer object', () => {
-//     const employee1 = new Engineer('github user'); 
-
-//     expect(employee1).toBeInstanceOf(Engineer); 
-
-// })
+test("Gets Github username with getGithub()", () => {
+    const testHub = "GitUserName";
+    const employee1 = new Engineer("Yoda", 2, "testing@test.com", testHub);
+    expect(employee1.getGithub()).toBe(testHub);
+});
