a17bd048670839899002fe82f4d62f312a6a1eef
peter <ptmartinezsound@gmail.com>
2021-04-23 17:33:17 -0500

    index inquiries

--- index.js
+++ index.js
@@ -49,9 +49,136 @@ const managerQuestions = [
             return true;
         }
     },
+
+    {
+        type: "input",
+        name: "office number",
+        message: "Please enter your office Number",
+        validate: async (input) => {
+            if (isNaN(input)) {
+                return "Please enter your office number";
+            }
+            return true;
+        }
+    },
 ]
 
 const employeeQuestions = [
 
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter an employee name",
+        validate: async (input) => {
+            if (input == "") {
+                return ("Please enter a name.");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter an employee email",
+        validate: async (input) => {
+            if (input == "") {
+                return ("Please enter an email address.");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter an employee id",
+        validate: async (input) => {
+            if (isNaN(input)) {
+                return ("Please enter a valid id.");
+            }
+            return true;
+        }
+    },
+
+    {
+        type: "list",
+        name: "role",
+        message: "what is employee's role in the company?",
+        choices: ["Engineer", "Intern"]
+    },
+
+    {
+        when: input => {
+            return input.role = "Engineer"
+        },
+
+        type: "input",
+        name: "github",
+        message: "Please enter the employee's github username",
+        validate: async (input) => {
+            if (input == "") {
+                return "Please enter a username"
+            }
+            return true;
+        }
+    },
+
+    {
+        when: input => {
+            return input.role = "Intern"
+        },
+
+        type: "input",
+        name: "school",
+        message: "Please enter the employee's school",
+        validate: async (input) => {
+            if (input == "") {
+                return "Please enter a school name"
+            }
+            return true;
+        }
+    },
+    
+    {
+        type: "confirm",
+        name: "add team member",
+        message: "Add another team member?",
+        choices: ["Yes, No"]
+    }
 ]
 
+function TeamBuild() {
+    inquirer.prompt(employeeQuestions).then(employeeInfo => {
+        if (employeeInfo.role == "Engineer") {
+            var newMember = new Engineer(employeeInfo.name, employeeInfo.length, employeeInfo.email, employeeInfo.gitHub);
+        } else {
+            var newMember = new Intern(employeeInfo.name, employeeInfo.length, employeeInfo.email, employeeInfo.school);
+        }
+        Team.push(newMember);
+        if (employeeInfo.addTeam === "yes") {
+            TeamBuild();
+        } else {
+            // htmlBuild();
+        }
+    })
+}
+
+
+
+
+
+
+function init() {
+    inquirer.prompt(managerQuestions).then(managerInfo => {
+        let teamManager = new Manager(managerInfo.name, 1, managerInfo.email, managerInfo.officeNumber);
+        Team.push(teamManager);
+        if (managerInfo.teamConfirm === "yes") {
+            TeamBuild();
+        } else {
+            // htmlBuild();
+        }
+    })
+}
+
+init();
