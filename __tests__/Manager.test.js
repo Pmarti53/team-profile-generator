ab710f214fb963b155c3a214522c1c382b598057
peter <ptmartinezsound@gmail.com>
2021-04-23 13:00:50 -0500

    completed manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,20 @@
-const Manager = require('../lib/Manager'); 
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
 
-// test('creates a Manager object', () => {
-//     const employee1 = new Manager(123); 
+test("Sets office number constructor", () => {
+    const testOffice = 213;
+    const employee1 = new Manager("Yaddle", 7, "testing@test.com", testOffice);
+    expect(employee1.officeNumber).toBe(testOffice);
+});
 
-//     expect(employee1).toBeInstanceOf(Manager); 
+test("getRole() returns \"Manager\"", () => {
+    const testName = "Manager";
+    const employee1 = new Manager("Yaddle", 7, "testing@test.com", testName);
+    expect(employee1.getRole()).toBe(testName);
+});
 
-// })
+test("Gets office number with getOfficeNumber()", () => {
+    const testOffice = 213;
+    const employee1 = new Manager("Yaddle", 7, "testing@test.com", testOffice);
+    expect(employee1.getOfficeNumber()).toBe(testOffice);
+});
