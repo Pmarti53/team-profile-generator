12465bf45c0ad47717c7b758883f163d56348cb4
peter <ptmartinezsound@gmail.com>
2021-04-23 12:44:26 -0500

    added intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,8 +1,19 @@
 const Intern = require('../lib/Intern'); 
 
-// test('creates a Intern object', () => {
-//     const employee1 = new Intern('highschill'); 
+test("Sets school constructor", () => {
+    const testSchool = "UTEP";
+    const employee1 = new Intern("MaceWindu", 6, "testing@test.com", testSchool);
+    expect(employee1.school).toBe(testSchool);
+});
 
-//     expect(employee1).toBeInstanceOf(Intern); 
+test("getRole() returns \"Intern\"", () => {
+    const testName = "Intern";
+    const employee1 = new Intern("MaceWindu", 6, "testing@test.com", testName);
+    expect(employee1.getRole()).toBe(testName);
+});
 
-// })
+test("Gets school name with getSchool()", () => {
+    const testSchool = "UTEP";
+    const employee1 = new Intern("MaceWindu", 6, "testing@test.com", testSchool);
+    expect(employee1.getSchool()).toBe(testSchool);
+});
